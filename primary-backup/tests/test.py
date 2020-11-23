50dbd60b602e475847cab0ecec9be402c33dd780
SadiJr <sadijacinto@gmail.com>
2020-11-23 17:41:51 -0300

    Upload test files

--- primary-backup/tests/test.py
+++ primary-backup/tests/test.py
@@ -1,8 +1,19 @@
-import sys
 import os
 
 print("Iniciando testes")
 
-print("Testando upload")
+print("Testando upload...")
+os.system('python front_end.py -u files' + os.path.sep + 't1.txt -s')
+os.system('python front_end.py -u files' + os.path.sep + 't2.txt -s')
 
-os.system('python front_end.py -u files/t1.txt -s')
+print("Testando update...")
+#Atualizar arquivos aqui
+
+for i in os.listdir('tests'):
+    f = open(i, 'wb')
+    f.write(b'Adicionando mais linhas para update')
+    f.close()
+
+print('Testando delete...')
+#Idem
+
